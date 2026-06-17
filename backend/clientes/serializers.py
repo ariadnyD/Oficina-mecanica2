873cacf30fab64f4aff1101362c2745331837010
José Salustiano <josesalustianojr@gmail.com>
2026-06-17 12:59:23 -0300

    feat: alterando serializers para cliente reconhecer CPF como ID

--- backend/clientes/serializers.py
+++ backend/clientes/serializers.py
@@ -1,9 +1,10 @@
-# serializers.py
 from rest_framework import serializers
 from .models import Cliente
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
-        fields = ['id', 'nome', 'cpf', 'data_nascimento', 'endereco', 'telefone', 'is_active']
-        read_only_fields = ['id', 'is_active'] # Impede que o usuário altere o status de exclusão manualmente via API
+        # Remova o 'id' da lista de campos
+        fields = ['nome', 'cpf', 'data_nascimento', 'endereco', 'telefone', 'is_active']
+        # Remova o 'id' daqui também
+        read_only_fields = ['is_active']
