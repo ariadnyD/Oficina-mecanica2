dfa9086392c29476faec8683d58d48b21de29582
José Salustiano <josesalustianojr@gmail.com>
2026-06-17 12:58:08 -0300

    feat: alterando serializers para veículo reconhecer o ID de cliente agora é o CPF

--- backend/veiculos/serializers.py
+++ backend/veiculos/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Veiculo
+from clientes.models import Cliente
 
 class VeiculoSerializer(serializers.ModelSerializer):
+    # O PrimaryKeyRelatedField entende que a PK de Cliente é o CPF
+    cliente = serializers.PrimaryKeyRelatedField(queryset=Cliente.objects.all())
+    
     class Meta:
         model = Veiculo
         fields = ['id', 'marca', 'modelo', 'tipo', 'cor', 'placa', 'cliente', 'ano']
-        read_only_fields = ['id']
