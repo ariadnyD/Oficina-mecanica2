e53e1e6fa6a9bd768cf53934096ecc0da437d1bd
José Salustiano <josesalustianojr@gmail.com>
2026-07-01 22:46:34 -0300

    feat: para que apareça o proprietário do veículo no DetalhesVeiculo

--- backend/veiculos/serializers.py
+++ backend/veiculos/serializers.py
@@ -5,7 +5,9 @@ from clientes.models import Cliente
 class VeiculoSerializer(serializers.ModelSerializer):
     # O PrimaryKeyRelatedField entende que a PK de Cliente é o CPF
     cliente = serializers.PrimaryKeyRelatedField(queryset=Cliente.objects.all())
+    cliente_nome = serializers.ReadOnlyField(source='cliente.nome')
+    cliente_cpf = serializers.ReadOnlyField(source='cliente.cpf')
     
     class Meta:
         model = Veiculo
-        fields = ['id', 'marca', 'modelo', 'tipo', 'cor', 'placa', 'cliente', 'ano']
+        fields = ['id', 'marca', 'modelo', 'tipo', 'cor', 'placa', 'cliente', 'ano', 'cliente_nome', 'cliente_cpf']
